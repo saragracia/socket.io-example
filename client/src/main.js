0dba13e982941c428a643a0b8a7bfc775e555f6b
sgracia <sgracia@alpify.com>
2015-07-02 13:20:24 +0200

    aperecen monedas-colision-desaparecen working en cliente

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,7 @@ var socket = require('socket.io-client')(serverURL)
 
 // You can use either `new PIXI.WebGLRenderer`, `new PIXI.CanvasRenderer`, or `PIXI.autoDetectRenderer`
 // which will try to choose the best renderer for the environment you are in.
-var renderer = new PIXI.WebGLRenderer(800, 600);
+var renderer = new PIXI.autoDetectRenderer(800, 600);
 
 // The renderer will create a canvas element for you that you can then insert into the DOM.
 document.body.appendChild(renderer.view);
@@ -15,10 +15,12 @@ document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
 
 // This creates a texture from a 'bunny.png' image.
-var bunnyTexture = PIXI.Texture.fromImage('bunny.png');
+var bunnyTexture = PIXI.Texture.fromImage('bunny-cage.png');
 var bunny = new PIXI.Sprite(bunnyTexture);
 global.bunny = bunny
+var coinTexture = PIXI.Texture.fromImage('yellow-coin.png');
 var otherBunnies = {}
+var coins = {}
 var bunnySpeed = 5
 
 // Setup the position and scale of the bunny
@@ -36,26 +38,41 @@ function animate() {
     // start the timer for the next animation loop
     requestAnimationFrame(animate);
 
-    /*
-    var oldPos = {
-      x: bunny.position.x,
-      y: bunny.position.y
-    }
-    */
-    var oldPos = bunny.position.clone()
-
-    // move bunny using keyboard keys
-    if (keyboard.char('W')) bunny.position.y -= bunnySpeed
-    if (keyboard.char('A')) bunny.position.x -= bunnySpeed
-    if (keyboard.char('D')) bunny.position.x += bunnySpeed
-    if (keyboard.char('S')) bunny.position.y += bunnySpeed
-
-    if (oldPos.x != bunny.position.x || oldPos.y != bunny.position.y) {
-      socket.emit('update_position', bunny.position)
-    }
-
+    if (keyboard.char('W')) moveY(-bunnySpeed); socket.emit('update_position', bunny.position)
+    if (keyboard.char('S')) moveY(+bunnySpeed); socket.emit('update_position', bunny.position)
+    if (keyboard.char('A')) moveX(-bunnySpeed); socket.emit('update_position', bunny.position)
+    if (keyboard.char('D')) moveX(+bunnySpeed); socket.emit('update_position', bunny.position)
+    
     // this is the main render call that makes pixi draw your container and its children.
     renderer.render(stage);
+}
+
+function moveY(incY) {
+    bunny.position.y += incY
+
+    for(coinId in coins) {
+      if (isColision(bunny.position, coins[coinId])) {
+        var sprite = coins[coinId]
+        if(sprite) stage.removeChild(sprite)
+        delete coins[coinId]
+      }
+    }
+}
+function moveX(incX) {
+    bunny.position.x += incX
+
+    for(coinId in coins) {
+      if (isColision(bunny.position, coins[coinId])) {
+        var sprite = coins[coinId]
+        if(sprite) stage.removeChild(sprite)
+        delete coins[coinId]
+      }
+    }
+}
+
+function isColision(posBunny, posCoin) {
+  if(Math.abs(posBunny.x - posCoin.x) < 64 && Math.abs(posBunny.y - posCoin.y) < 64) return true
+  return false
 }
 
 socket.on('update_position', function (pos) {
@@ -70,6 +87,21 @@ socket.on('update_position', function (pos) {
   }
   sprite.position.x = pos.x
   sprite.position.y = pos.y
+})
+
+socket.on('player_disconnected', function (id) {
+  var sprite = otherBunnies[id]
+  if(sprite) stage.removeChild(sprite)
+  delete otherBunnies[id]
+})
+
+socket.on('put_pick_up', function (pos) {
+  var sprite = new PIXI.Sprite(coinTexture)
+  stage.addChild(sprite)
+  sprite.anchor.set(0.5,0.5)
+  sprite.position = pos
+  pos.id = pos.x + '' + pos.y
+  coins[pos.id] = sprite
 })
 
 socket.on('connect', function () {
--- server/index.js
+++ server/index.js
@@ -1,16 +1,36 @@
 var port = process.env.PORT || 9000
 var io = require('socket.io')(port)
 
+var bunnies = {}
+var coins 	= {}
+
 io.on('connection', function (socket) {
   socket.broadcast.emit('hi')
   console.log('connection', socket.id)
+  for(var bunnyId in bunnies) {
+  	socket.emit('update_position', bunnies[bunnyId])
+  }
   socket.on('disconnect', function () {
     console.log('disconnection', socket.id)
+    delete bunnies[socket.id]
+    socket.broadcast.emit('player_disconnected', socket.id)
   })
   socket.on('update_position', function (pos) {
     pos.id = socket.id
+    bunnies[socket.id] = pos
     socket.broadcast.emit('update_position', pos)
   })
 })
+
+setInterval(putPickUp, 5000);
+
+function putPickUp() {
+	var pos = {}
+	pos.x = Math.random() * 800
+	pos.y = Math.random() * 600
+	pos.id = pos.x + '' + pos.y
+	coins[pos.id] = pos
+	io.sockets.emit('put_pick_up', pos)
+}
 
 console.log('server started on port', port)
